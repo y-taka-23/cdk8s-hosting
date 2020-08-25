e35801c078cd39afdb7025c97ac3e0ad000adcb2
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2020-08-26 00:39:36 +0900

    Add an example

--- example/example.ts
+++ example/example.ts
@@ -1,12 +1,20 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
 import { Hosting } from '../lib/index';
+import * as path from 'path';
 
 export class MyChart extends Chart {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    new Hosting(this, 'mypage');
+    // default options
+    new Hosting(this, 'default');
+
+    // explicit options
+    new Hosting(this, 'explicit', {
+      host: 'www.example.com',
+      public: path.join(__dirname, 'public')
+    });
   }
 }
 
